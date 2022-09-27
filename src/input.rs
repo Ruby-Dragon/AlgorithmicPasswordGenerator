f0ed6369b45ae3839bd80af28983d802da3eb9fb
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-09-27 10:05:36 -0700

    add algorithm choice thing

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,13 @@
 pub mod input{
-    pub fn get_words() -> Vec<String> {
+
+    pub struct PasswordInput{
+        pub words:Vec<String>,
+        pub algorithms:Vec<i32>
+    }
+
+    pub fn get_words() -> PasswordInput {
         let mut output_vector = Vec::new();
+        let mut algorithm_vector = Vec::new();
         'word: loop {
             let mut word = String::new();
             std::io::stdin().read_line(&mut word).unwrap();
@@ -10,8 +17,22 @@ pub mod input{
                 break 'word;
             }
             output_vector.push(word);
-        }
 
-        return output_vector;
+            println!("What algorithm should be used (1-3)? ");
+            let mut number_input= String::new();
+            std::io::stdin().read_line(&mut number_input).unwrap();
+            match number_input.trim() {
+                "1" => algorithm_vector.push(1),
+                "2" => algorithm_vector.push(2),
+                "3" => algorithm_vector.push(3),
+                _ => {println!("That is not a recognised input, defaulting to 1"); algorithm_vector.push(1)},
+            }
+        }
+        let finished = PasswordInput {
+            words : output_vector,
+            algorithms : algorithm_vector
+        };
+
+        return finished;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ fn main() {
     println!("Hello, world!");
     let mut output_vector = input::input::get_words();
 
-    for x in &output_vector{
+    for x in &output_vector.words{
+        println!("{x}");
+    }
+
+    for x in &output_vector.algorithms{
         println!("{x}");
     }
 }
