03f2a381a540b86980946f364aa75abd529238fc
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-10-03 09:38:12 -0700

    finish program prompts

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # AlgorithmicPasswordGenerator
 Generates a memorable password using a simple algorithm and user input
 
+## Install
+
+Use Cargo on this repository.
+
+## Usage
+
+This is a command line program, run from shell with cargo run in the repo or if built, ./AlgorithmicPasswordGenerator
+
+
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,8 @@ mod algorithms;
 
 fn main() {
     println!("Algorithmic Password Generator. Enter -d when done.");
-    let mut output_vector = input::input::get_words();
+    let mut input_words = input::input::get_words();
 
-    for x in &output_vector.words{
-        println!("{x}");
-    }
-
-    for x in &output_vector.algorithms{
-        println!("{x}");
-    }
-
-    println!("{}", choose::choose::run_algorithms(output_vector))
+    println!("Generated Password:");
+    println!("{}", choose::choose::run_algorithms(input_words));
 }
